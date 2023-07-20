1abe91f092fccecc12fb5d0ebb4b5433175d6591
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-07-21 01:41:46 +0200

    Ejercicio 2 JS

---
+++ JS WILMER/es-2/exercise.js
@@ -0,0 +1,13 @@
+let name= 'Mario';
+let surname = 'Rossi';
+let age = 20;
+let isMarried = false;
+let job = 'Developer';
+let isGraduated= false;
+
+console.log (name + "\n" +
+surname + "\n" +
+age + "\n" +
+isMarried + "\n" +
+job + "\n" +
+isGraduated)
