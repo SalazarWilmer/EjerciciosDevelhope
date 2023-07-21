df6fc4864b37fe6ff97611b23f78c833609e5fe7
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-07-21 19:49:58 +0200

    Ejercicio 23 JS

---
+++ JS WILMER/es-23/exercise.js
@@ -0,0 +1,13 @@
+let roomType = "Junior Suite"
+
+switch (roomType) {
+    case 'Basic Room':
+      console.log('Price for Basic Room --> 50€');
+      break;
+    case 'Junior Suite':
+      console.log('Price for Junior Suite Room --> 80€');
+      break;
+    case 'Master Suite':
+      console.log('Price for Master Suite  --> 100€');
+      break;
+}
