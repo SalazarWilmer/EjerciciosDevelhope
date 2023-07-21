e4a9ec136de25dee68743537cfee2ae42cc92423
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-07-21 21:03:29 +0200

    Ejercicio 30 JS

---
+++ JS WILMER/es-30/exercise.js
@@ -0,0 +1,10 @@
+function sayHelloName(name) {
+    const greeting = "Hello " + name;
+    return greeting;
+  }
+  
+  
+  const name = "Cosimo";
+  const result = sayHelloName(name);
+  console.log(result);
+  
