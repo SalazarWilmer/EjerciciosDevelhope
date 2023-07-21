5fb0a819324af927fcf656145a60a2238221585d
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-07-21 02:07:31 +0200

    Ejercicio 7 JS

---
+++ JS WILMER/es-7/exercise.js
@@ -0,0 +1,10 @@
+let hello = 'Ciao';     // number
+let age = 18;       // boolean
+let isGraduated = false;     // string
+
+
+console.log(Number(hello));
+console.log(Boolean(age));
+console.log(String(isGraduated));
+
+// NaN significa que no es un numero o Not a Number por eso imprime eso
