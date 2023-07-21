be0a6be244f2258dad99a064d26d46954b56e1b3
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-07-21 17:07:12 +0200

    Ejercicio 18 JS

---
+++ JS WILMER/es-18/exercise.js
@@ -0,0 +1,13 @@
+let number = 7;
+
+if (number > 0) {
+
+  console.log("The number is positive");
+  
+} else console.log("The number is negative");
+
+
+
+//ternary operator
+let resultado = number > 0 ? "The number is positive" : "The number is negative"; 
+ console.log(resultado)
