576c6125a2d35012f080da7d7b867dc251159f9c
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-07-21 03:05:09 +0200

    Ejercicio 10 JS

---
+++ JS WILMER/es-10/exercise.js
@@ -0,0 +1,11 @@
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+yearsCompleted == firstYearCompleted
+yearsCompleted === firstYearCompleted
+
+console.log(yearsCompleted == firstYearCompleted)
+console.log(yearsCompleted === firstYearCompleted)
+
+//la diferencia entre el == y el === es que el primero solo toma en consideracion el dato mas no el tipo de dato, mientras
+//que el segundo toma en consideracion el dato y el tipo de dato suministrado a la hora de realizar la comparacion
