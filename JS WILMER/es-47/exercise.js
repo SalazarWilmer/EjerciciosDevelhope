d7c4c4c68348feec50c2dd0da3b66cd016e6db71
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-07-28 13:14:57 +0200

    Ejercicio 47 JS

---
+++ JS WILMER/es-47/exercise.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers[0]);
+
+console.log(numbers[numbers.length - 1]);
+
+console.log(numbers.length);
+
+console.log(numbers[19]);
+//El array solo posee 10 elementos, con index que van del 0 al 9, JS es un lenguaje que esta basado en 0 por eso el elmento 20 esta en el indice 19
+//y la salida es undefined porque no existe ese elemento ya que esta fuera del rango del array.
+
