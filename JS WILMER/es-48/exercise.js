d6702cf329f9ef795de290643f35f8609c403053
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-07-28 13:21:20 +0200

    Ejercicio 48 JS

---
+++ JS WILMER/es-48/exercise.js
@@ -0,0 +1,22 @@
+const students = [
+  {
+    id: 1,
+    name: "Evelyn",
+    surname: "Smith",
+    age: 29,
+  },
+  {
+    id: 2,
+    name: "Laura",
+    surname: "Hernandez",
+    age: 19,
+  },
+  {
+    id: 3,
+    name: "Jose",
+    surname: "Lara",
+    age: 26,
+  },
+];
+
+console.log(students[0]);
