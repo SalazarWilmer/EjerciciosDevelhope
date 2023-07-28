da1320b7187d4748febfc0947af156eaa62a7be7
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-07-28 13:07:50 +0200

    Ejercicio 46 JS

---
+++ JS WILMER/es-46/exercise.js
@@ -0,0 +1,13 @@
+const student = {
+  id: 1,
+  school: "Liceo",
+  year: 3,
+};
+
+student.personalData = {
+  name: "Pepito",
+  surname: "Moya",
+  age: 18,
+};
+
+console.log(student.personalData);
