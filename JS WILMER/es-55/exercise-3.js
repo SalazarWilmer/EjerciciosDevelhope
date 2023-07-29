03445618418ca9be757b4c3af679fc3c2dbb1820
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-07-29 17:47:36 +0200

    Ejercicio 55 JS

---
+++ JS WILMER/es-55/exercise-3.js
@@ -0,0 +1,24 @@
+function ageAverage(people) {
+  const totalAge = people.reduce((sum, person) => sum + person.age, 0);
+
+  const average = totalAge / people.length;
+
+  return average;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = ageAverage(persons);
+console.log(persons);
+console.log(average);
