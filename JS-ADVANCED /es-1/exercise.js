9568eefa74ab92c490855263920c74fb2c88922e
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-08-01 08:04:22 +0200

    Ejercicio 1 JS ADVANCED

--- JS-ADVANCED /es-1/exercise.js
+++ JS-ADVANCED /es-1/exercise.js
@@ -1,19 +1,10 @@
+// Class definition
 class Person {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+    constructor(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
   }
-}
-
-class Developer extends Person {
-  constructor (firstName, lastName, role) {
-    super(firstName, lastName);
-    this.role = role;
-  }
-}
-
-
-const developer = new Developer("Mario", "Rossi", "Front-end");
-console.log(
-  developer.firstName + " " + developer.lastName + " " + developer.role
-);
+  
+const developer = new Person('Mario', 'Rossi');
+console.log(developer.firstName + " " + developer.lastName);
