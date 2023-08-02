72a627f80b26b44d4cca983cc1d414fdb08c5e60
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-08-02 18:44:28 +0200

    Ejercicio 13 JS ADVANCED

---
+++ JS-ADVANCED /es-13/exercise.js
@@ -0,0 +1,10 @@
+function sum(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+const numbers = [1, 2, 3];
+// console.log(sum(numbers[0], numbers[1], numbers[2]));
+
+// usando spread operator
+console.log(sum(...numbers));
+
