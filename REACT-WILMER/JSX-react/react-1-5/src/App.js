b23524fa2fd6d2a7e249f84a5470df726da7ea4e
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-09-23 03:51:08 +0200

    Ejercicio JSX 5

--- REACT-WILMER/JSX-react/react-1-5/src/App.js
+++ REACT-WILMER/JSX-react/react-1-5/src/App.js
@@ -3,6 +3,7 @@ import JSX1 from "./Ejercicios/JSX1";
 import JSX2 from "./Ejercicios/JSX2";
 import JSX3 from "./Ejercicios/JSX3";
 import JSX4 from "./Ejercicios/JSX4";
+import JSX5 from "./Ejercicios/JSX5";
 
 
 
@@ -14,7 +15,9 @@ function App() {
       <JSX2/>
       <JSX3/>
       <JSX4 name={<a href="https://google.com">Ejemplo</a>} />
-      
+      <JSX5 
+      a={16}
+      b={24} />
     </div>
   );
 }
---
+++ REACT-WILMER/JSX-react/react-1-5/src/Ejercicios/JSX5.js
@@ -0,0 +1,9 @@
+import React from "react";
+// Write a function that receives two parameters, a and b and returns the sum of them within an h2 tag.
+
+const JSX5 = ({a, b }) => {
+    const sum = a + b;
+    return <h2>Sum: {sum}</h2>;
+  };
+  
+export default JSX5;
