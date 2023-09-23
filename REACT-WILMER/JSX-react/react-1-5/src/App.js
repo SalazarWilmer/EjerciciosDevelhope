20450886e525cb580a09d650904e7eb39d298929
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-09-23 03:03:32 +0200

    Ejercicio JSX 1

--- REACT-WILMER/JSX-react/react-1-5/src/App.js
+++ REACT-WILMER/JSX-react/react-1-5/src/App.js
@@ -1,32 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import JSX1 from "./Ejercicios/JSX1";
+
+
+
 
 function App() {
-  const hello = <h1>Hello, World!</h1>;
-  const diHola = (name) => {
-    // return <h1>¡Hola, {name}!</h1>;
-  // };
-
-  // para verificar si name está definido. Si está definido, muestra "¡Hola, {name}!",
-  //  de lo contrario, muestra "¡Hola, undefined!". De esta manera, si no se pasa un nombre,
-  //  mostrará "¡Hola, undefined!"
-  // y si solamente se deja asi nada dentro de los parentesis simplemente arroja el string ¡Hola, !
-  // siendo este su return sin el if return <h1>¡Hola, {name}!</h1>;
-
-    if (name) {
-      return <h1>¡Hola, {name}!</h1>;
-    } else {
-      return <h1>¡Hola, undefined!</h1>;
-    }
-
-  };
-
-  const hi = diHola();
-
   return (
-    <div>
-      {hello}
-      {hi}
+    <div className="App">
+      <JSX1/>
+
     </div>
   );
 }
---
+++ REACT-WILMER/JSX-react/react-1-5/src/Ejercicios/JSX1.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const hello = <h1>Hello, World!</h1>;
+const JSX1 = () => {
+  return <div>{hello}</div>;
+};
+
+export default JSX1;
