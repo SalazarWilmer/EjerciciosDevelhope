c4946bc80cc1b806000ae5766a72acaab97ace89
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-09-26 11:14:27 +0200

    Ejercicio Props 17

--- REACT-WILMER/proyecto-vite/vite-project/src/App.jsx
+++ REACT-WILMER/proyecto-vite/vite-project/src/App.jsx
@@ -2,13 +2,12 @@ import Hello from "./Ejercicios/Hello";
 import Message from "./Ejercicios/Message";
 import Welcome from "./Ejercicios/Welcome";
 function App() {
-  // const name = "Wilmer";
   return (
     <div className="App">
       <Hello />
       <Message />
       <Hello />
-      <Welcome name={"Wilmer"} />
+      <Welcome name={''} />
     </div>
   );
 }
--- REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/Welcome.jsx
+++ REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/Welcome.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 const Welcome = (props) => {
   return (
     <p>
-      Welcome, {props.name}!
+      Welcome, {props.name || 'guest'}!
     </p>
   );
 }
+
+
+/**Respuesta al ejercicio 17 de props: Como podemos observar si se puede establecer 
+ * un valor predeterminado si no se proporciona el nombre en la variable name, de lo contrario el valor que 
+ * mostraria la variable seria undefined */
 export default Welcome;
