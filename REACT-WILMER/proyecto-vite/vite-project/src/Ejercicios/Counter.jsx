ae4a476a7e7cff6bdb16e8c3744119e3ba6a5b34
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-09-28 15:08:32 +0200

    Ejercicio State 30

--- REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/Counter.jsx
+++ REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/Counter.jsx
@@ -16,3 +16,13 @@ const Counter = () => {
 };
 
 export default Counter;
+
+
+/**
+ * Respuesta al ejercicio 30:
+ * En este componente estamos usando una funcion de valor inmediato, tambien podriamos hacerlo con una funcion que devuelva
+ * un valor que podria ser setCounter(prevCounter => prevCounter + 1), de las dos maneras se obtiene el resultado deseado, lo 
+ * que hay que tener en cuenta es que a medida que vayamos realizando mas y mas proyectos y ejercicios tendremos diferentes 
+ * situaciones en las que podamos usar una manera y la otra, la diferencia entre estas funciones es que la primera te dara un
+ * valor directo mientras que la segunda tomara un valor y le sumara a ese valor dado.
+ */
