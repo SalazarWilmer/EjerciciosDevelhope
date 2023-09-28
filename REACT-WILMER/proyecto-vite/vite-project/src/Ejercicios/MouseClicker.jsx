9faf3b6cc62ff70042bc5afc88b3cafc4c631f38
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-09-28 18:08:45 +0200

    Ejercicio Handling Events 39

--- REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/MouseClicker.jsx
+++ REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/MouseClicker.jsx
@@ -4,9 +4,13 @@ const MouseClicker = ({ name }) => {
   const handleClick = (event) => {
     console.log("Boton click:", event.target.name);
   };
-  const handleImageClick = (event) => {
-    console.log("Img click, src:", event.target.src);
-};
+//   const handleImageClick = (event) => {
+//     console.log("Img click, src:", event.target.src);
+// };
+const handleImageClick = (event) => {
+    console.log("Imagen clicada, src:", event.target.src);
+    event.stopPropagation(); 
+  };
   return (
     <button name={name} onClick={handleClick}>
       <img src="1.jpg" alt="pics" 
