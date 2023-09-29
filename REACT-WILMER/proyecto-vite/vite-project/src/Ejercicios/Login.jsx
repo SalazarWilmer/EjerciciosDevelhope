44df4019f94b559c99a50e6ec256e3620f9a5c20
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-09-29 12:55:46 +0200

    Ejercicio Uncontrolled Forms 48

--- REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/Login.jsx
+++ REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/Login.jsx
@@ -19,10 +19,18 @@ const Login = ({ onLogin }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault(); 
+
+    const formData = new FormData(event.target);
+
+    const username = formData.get('username');
+    const password = formData.get('password');
+    const remember = formData.get('remember') === 'on';
+
     if (username && password) {
       onLogin({ username, password, remember });
     }
   };
+
   const handleReset = () => {
     setUsername("");
     setPassword("");
@@ -36,6 +44,7 @@ const Login = ({ onLogin }) => {
         <input
           type="text"
           id="username"
+          name="username" 
           value={username}
           onChange={handleUsernameChange}
         />
@@ -45,6 +54,7 @@ const Login = ({ onLogin }) => {
         <input
           type="password"
           id="password"
+          name="password" 
           value={password}
           onChange={handlePasswordChange}
         />
@@ -53,10 +63,11 @@ const Login = ({ onLogin }) => {
         <label>
           <input
             type="checkbox"
+            name="remember" 
             checked={remember}
             onChange={handleRememberChange}
           />
-          Remember Me
+          Remember
         </label>
       </div>
       <button type="submit" disabled={!username || !password}>
@@ -68,3 +79,17 @@ const Login = ({ onLogin }) => {
 };
 
 export default Login;
+
+
+/**
+ * Respuesta al ejercicio Uncontrolled Forms 48:
+ * 
+ * Are there non-standard APIs that can be used to access the form data? R= Las APIs no estandar son donde utlizamos document.getElementById, no es tan recomendable hacerlo de esta manera ya que existen 
+ * maneras mas actualizadas y mejor legibles ahora.
+ * 
+ *  What is the standard API? R=Es una interfaz de programación de aplicaciones que sigue un conjunto 
+ * establecido de especificaciones y reglas ampliamente aceptadas. Estas especificaciones están definidas y 
+ * mantenidas por organizaciones reconocidas y suelen estar disponibles públicamente para que los desarrolladores 
+ * las utilicen.
+ * 
+ */
