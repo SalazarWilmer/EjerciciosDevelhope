9d083ae66257b014830a059fac4e4d482664a139
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-09-29 12:42:09 +0200

    Ejercicio Uncontrolled Forms 47

--- REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/UncontrolledLogin.jsx
+++ REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/UncontrolledLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 
 const UncontrolledLogin = ({ onLogin }) => {
   const usernameRef = useRef(null);
@@ -18,8 +18,8 @@ const UncontrolledLogin = ({ onLogin }) => {
   };
 
   const handleReset = () => {
-    usernameRef.current.value = '';
-    passwordRef.current.value = '';
+    usernameRef.current.value = "";
+    passwordRef.current.value = "";
     rememberRef.current.checked = false;
   };
 
@@ -40,9 +40,51 @@ const UncontrolledLogin = ({ onLogin }) => {
         </label>
       </div>
       <button type="submit">Login</button>
-      <button type="button" onClick={handleReset}>Reset</button>
+      <button type="button" onClick={handleReset}>
+        Reset
+      </button>
     </form>
   );
 };
 
 export default UncontrolledLogin;
+/** Respuesta al ejercicio Uncontrolled Forms 47:
+*Existen 3 maneras de acceder a la data de form de los valores actuales de username y password
+n1= Primero se puede hacer utilizando componentes controlados aqui los campos de entrada estan vinculados al useState de React
+y se puede acceder directamente desde las variables del estado ej:
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+
+const nombreUsuarioActual = username;
+const contraseñaActual = password;
+ */
+
+/**
+n2 = La segunda manera es utilizando Refs con componentes no controlados para acceder a los nodos del DOM y a sus valores actuales.
+Ej: 
+const refUsuario = useRef(null);
+const refContraseña = useRef(null);
+
+const nombreUsuarioActual = refUsuario.current.value;
+const contraseñaActual = refContraseña.current.value;
+
+<input type="text" ref={refUsuario} />
+<input type="password" ref={refContraseña} />
+*/
+
+/**
+n3 = La tercera manera es utilizando Objetos de Evento en Manejadores de Eventos donde puedes acceder  a los valores desde el objeto
+de evento que se pasa al manejador de eventos. 
+Ej:
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const nombreUsuarioActual = event.target.elements.username.value;
+  const contraseñaActual = event.target.elements.password.value;
+};
+
+Hay que tener en cuenta que el enfoque adecuado dependerá de si estás usando componentes controlados o no controlados y
+ de los requisitos específicos de tu aplicación. Los componentes controlados están más centrados en React
+y se gestionan a través del estado, mientras que los componentes no controlados interactúan de manera 
+más directa con el DOM.
+*/
