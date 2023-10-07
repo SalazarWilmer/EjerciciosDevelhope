c9cb63886334a723a8344a74d7a60fffcf6e50cf
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-10-07 14:23:09 +0200

    Ejercicio Ref 52

--- REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/Counter.jsx
+++ REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/Counter.jsx
@@ -1,11 +1,20 @@
-import React, { useState, useEffect } from "react";
-import CounterDisplay  from "./CounterDisplay";
+import React, { useState, useEffect, useRef } from "react";
+import CounterDisplay from "./CounterDisplay";
 
-const Counter = ({ initialValue, incrementAmount, decrementAmount  }) => {
+const Counter = ({ initialValue, incrementAmount, decrementAmount }) => {
   const [counter, setCounter] = useState(initialValue);
+  const cambiarDireccionRef = useRef(null);
+
   useEffect(() => {
-    console.log("Valor actual del contador:", counter);
-  }, [counter]);
+    if (counter > initialValue) {
+      cambiarDireccionRef.current = "up";
+    } else if (counter < initialValue) {
+      cambiarDireccionRef.current = "down";
+    }
+    if (cambiarDireccionRef.current !== null) {
+      console.log("Dirección Cambiada", cambiarDireccionRef.current);
+    }
+  }, [counter, initialValue]);
   const incrementCounter = () => {
     setCounter(counter + incrementAmount);
   };
@@ -19,7 +28,7 @@ const Counter = ({ initialValue, incrementAmount, decrementAmount  }) => {
 
   return (
     <div>
-     <CounterDisplay count={counter} />
+      <CounterDisplay count={counter} />
       <button onClick={incrementCounter}>Incrementar</button>
       <button onClick={decrementCounter}>Decrementar</button>
       <button onClick={resetCounter}>Reiniciar</button>
@@ -29,12 +38,11 @@ const Counter = ({ initialValue, incrementAmount, decrementAmount  }) => {
 
 export default Counter;
 
-
 /**  setCounter(counter + 1); funcion valor inmediato
  * Respuesta al ejercicio 30:
  * En este componente estamos usando una funcion de valor inmediato, tambien podriamos hacerlo con una funcion que devuelva
- * un valor que podria ser setCounter(prevCounter => prevCounter + 1), de las dos maneras se obtiene el resultado deseado, lo 
- * que hay que tener en cuenta es que a medida que vayamos realizando mas y mas proyectos y ejercicios tendremos diferentes 
+ * un valor que podria ser setCounter(prevCounter => prevCounter + 1), de las dos maneras se obtiene el resultado deseado, lo
+ * que hay que tener en cuenta es que a medida que vayamos realizando mas y mas proyectos y ejercicios tendremos diferentes
  * situaciones en las que podamos usar una manera y la otra, la diferencia entre estas funciones es que la primera te dara un
  * valor directo mientras que la segunda tomara un valor y le sumara a ese valor dado.
  */
