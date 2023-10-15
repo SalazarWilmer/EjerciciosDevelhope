bb58c30d898a55a6da3ef671b7897efb70c104bd
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-10-15 15:08:34 +0200

    Ejercicio Component Composition 69

--- REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/Container.jsx
+++ REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/Container.jsx
@@ -1,13 +1,25 @@
-import React from "react";
-import "../styles/index.scss"
+import React, { useState } from "react";
+import "../styles/index.scss";
 
 const Container = ({ title, children }) => {
-    return (
-      <div className="container-co">
-        {title && <h2 className="text-xl mb-4">{title}</h2>}
-        {children}
-      </div>
-    );
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const handleToggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
   };
-  
+
+  return (
+    <div className={`container-co ${isCollapsed ? "collapsed" : ""}`}>
+      <div
+        className="flex justify-between cursor-pointer"
+        onClick={handleToggleCollapse}
+      >
+        {title && <h2 className="text-xl mb-4">{title}</h2>}
+        <span>{isCollapsed ? "▼" : "▲"}</span>
+      </div>
+      {!isCollapsed && children}
+    </div>
+  );
+};
+
 export default Container;
