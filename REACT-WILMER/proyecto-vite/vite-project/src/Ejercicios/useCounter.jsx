1272064eafdaf4241e66501fb4a0a07a0971c1dc
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-10-24 13:43:19 +0200

    Ejercicio useCallBack and useMemo 80

--- REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/useCounter.jsx
+++ REACT-WILMER/proyecto-vite/vite-project/src/Ejercicios/useCounter.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 const useCounter = (initialValue = 0, incrementAmount = 1, decrementAmount = 1) => {
   const [counter, setCounter] = useState(initialValue);
 
-  const increment = () => {
+  const increment = useCallback(() => {
     setCounter(prevCounter => prevCounter + incrementAmount);
-  };
+  }, [incrementAmount]);
 
-  const decrement = () => {
+  const decrement = useCallback(() => {
     setCounter(prevCounter => prevCounter - decrementAmount);
-  };
+  }, [decrementAmount]);
 
-  const reset = () => {
+  const reset = useCallback(() => {
     setCounter(initialValue);
-  };
+  }, [initialValue]);
 
   return { counter, increment, decrement, reset };
 };
