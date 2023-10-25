94648ced10d5684e2fd9901cb3d38ec95d09d299
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-10-25 16:46:19 +0200

    Ejercicio React Router 82

--- REACT-WILMER/proyecto-vite/vite-project/src/App.jsx
+++ REACT-WILMER/proyecto-vite/vite-project/src/App.jsx
@@ -1,28 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
-import Hello from "./Ejercicios/Hello";
-import Message from "./Ejercicios/Message";
+
 import Welcome from "./Ejercicios/Welcome";
-import Age from "./Ejercicios/Age";
-import AlertClock from "./Ejercicios/AlertClock";
-import Counter from "./Ejercicios/Counter";
-import Clock from "./Ejercicios/Clock";
-import MouseClicker from "./Ejercicios/MouseClicker";
-import MultiButton from "./Ejercicios/MultiButton";
-import InteractiveWelcome from "./Ejercicios/InteractiveWelcome";
-import Login from "./Ejercicios/Login";
-import UncontrolledLogin from "./Ejercicios/UncontrolledLogin";
-import FocusableInput from "./Ejercicios/FocusableInput";
-import FirstMount from "./Ejercicios/FirstMount";
-import Colors from "./Ejercicios/Colors";
-import TodoList from "./Ejercicios/TodoList";
-import Container from "./Ejercicios/Container";
-import { LanguageProvider } from "./Ejercicios/LanguageContext";
-import LanguageSelector from "./Ejercicios/LanguageSelector";
-import GithubUser from "./Ejercicios/GithubUser";
-import GithubUsers from "./Ejercicios/GithubUsers";
-import LoginForm from "./Ejercicios/LoginForm";
-import CurrentLocation from "./Ejercicios/CurrentLocation";
-import FilteredList from "./Ejercicios/FilteredList";
+
 
 function App() {
   const handleLogin = (userData) => {
@@ -60,44 +39,8 @@ function App() {
   ];
   return (
     <div className="App">
-      <LanguageProvider value={language}>
-        <LanguageSelector />
-        <Clock />
-      </LanguageProvider>
-      <Hello />
-      <Message />
-      <Hello />
-      <Age age={10} />
-      <Welcome name={name} age={40} />
-      <AlertClock onClickHandler={handleAlertClick} />
-      <Counter
-        initialValue={initialValue}
-        incrementAmount={incrementAmount}
-        decrementAmount={decrementAmount}
-      />
-      <MouseClicker name="one" />
-      <MultiButton />
-      <InteractiveWelcome />
-      <Login onLogin={handleLogin} />
-      <UncontrolledLogin onLogin={handleLogin} />
-      <FocusableInput />
-      <FirstMount />
-      <h1>Lista de colores</h1>
-      <Colors items={colorList} />
-      <TodoList />
-      <Container title="Mi primer Contenedor">
-        <p>Contenido dentro del container :P</p>
-      </Container>
-
-      <GithubUser username={username} />
-
-      <GithubUsers />
-
-      <LoginForm onLogin={handleLogin} />
-
-      <CurrentLocation />
-
-      <FilteredList list={studentsList} />
+      <Welcome  />
+      
     </div>
   );
 }
--- REACT-WILMER/proyecto-vite/vite-project/src/index.jsx
+++ REACT-WILMER/proyecto-vite/vite-project/src/index.jsx
@@ -1,6 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { createRoot } from 'react-dom/client';
 
+const root = document.getElementById("root");
 
-const root = createRoot(document.getElementById('root'))
-root.render(<App/>)
+const appRoot = ReactDOM.createRoot(root);
+appRoot.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
