d07839b6205e081069220751f176e423124e1e02
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-11-25 20:05:32 +0100

    Ejercicio 09 NODE

---
+++ NODE-WILMER/NodeProyect/script3.mjs
@@ -0,0 +1,40 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+  }
+  
+  luckyDraw('Joe')
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error(error.message);
+    })
+    .then(() => {
+      return luckyDraw('Stolas');
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error(error.message);
+    })
+    .then(() => {
+      return luckyDraw('Sabrina');
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+  
