aeba361968dbeef5afb66bad27dc80daddba0cf0
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-11-26 20:07:39 +0100

    Ejercicio 13 NODE

--- NODE-WILMER/NodeProyect/dist/server.js
+++ NODE-WILMER/NodeProyect/dist/server.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("express-async-errors");
 const morgan_1 = __importDefault(require("morgan"));
+const joi_1 = __importDefault(require("joi"));
 require("dotenv/config");
 let planets = [
     {
@@ -18,11 +19,50 @@ let planets = [
     },
 ];
 const app = (0, express_1.default)();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.use((0, morgan_1.default)("dev"));
-app.get("/", (req, res) => {
+app.use(express_1.default.json());
+const planetSchema = joi_1.default.object({
+    id: joi_1.default.number().required(),
+    name: joi_1.default.string().required(),
+});
+app.get("/api/planets", (req, res) => {
     res.status(200).json(planets);
 });
+app.get("/api/planets/:id", (req, res) => {
+    const { id } = req.params;
+    const planet = planets.find((p) => p.id === Number(id));
+    res.status(200).json(planet);
+});
+app.post("/api/planets", (req, res) => {
+    const { id, name } = req.body;
+    // Validate the request body
+    const validationResult = planetSchema.validate({ id, name });
+    if (validationResult.error) {
+        return res.status(400).json({ error: validationResult.error.message });
+    }
+    const newPlanet = { id, name };
+    planets = [...planets, newPlanet];
+    console.log(planets);
+    res.status(201).json({ msg: "El nuevo planeta fue creado." });
+});
+app.put("/api/planets/:id", (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const validationResult = planetSchema.validate({ id: Number(id), name });
+    if (validationResult.error) {
+        return res.status(400).json({ error: validationResult.error.message });
+    }
+    planets = planets.map((p) => (p.id === Number(id) ? Object.assign(Object.assign({}, p), { name }) : p));
+    console.log(planets);
+    res.status(200).json({ msg: "El planeta fue actualizado" });
+});
+app.delete("/api/planets/:id", (req, res) => {
+    const { id } = req.params;
+    planets = planets.filter((p) => p.id !== Number(id));
+    console.log(planets);
+    res.status(200).json({ msg: "El planeta fue eliminado." });
+});
 app.listen(port, () => {
-    console.log(`Se inicializa el puerto: http://localhost:${port}/`);
+    console.log(`Se inicializa el puerto: http://localhost:${port}`);
 });
--- NODE-WILMER/NodeProyect/src/server.ts
+++ NODE-WILMER/NodeProyect/src/server.ts
@@ -1,35 +1,89 @@
 import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
-import 'dotenv/config'
+import Joi from "joi";
+import "dotenv/config";
 
 type Planet = {
-    id: number;
-    name: string;
-  };
+  id: number;
+  name: string;
+};
 
-  type Planets = Planet[];
+type Planets = Planet[];
 
-  let planets: Planets = [
-    {
-      id: 1,
-      name: "Earth",
-    },
-    {
-      id: 2,
-      name: "Mars",
-    },
-  ];
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
 
 const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
+app.use(express.json());
 
-app.get("/", (req, res) => {
+const planetSchema = Joi.object({
+  id: Joi.number().required(),
+  name: Joi.string().required(),
+});
+
+app.get("/api/planets", (req, res) => {
   res.status(200).json(planets);
 });
 
+app.get("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const planet = planets.find((p) => p.id === Number(id));
+  res.status(200).json(planet);
+});
+
+app.post("/api/planets", (req, res) => {
+  const { id, name } = req.body;
+
+  // Validate the request body
+  const validationResult = planetSchema.validate({ id, name });
+  if (validationResult.error) {
+    return res.status(400).json({ error: validationResult.error.message });
+  }
+
+  const newPlanet = { id, name };
+  planets = [...planets, newPlanet];
+
+  console.log(planets);
+
+  res.status(201).json({ msg: "El nuevo planeta fue creado." });
+});
+
+app.put("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const validationResult = planetSchema.validate({ id: Number(id), name });
+  if (validationResult.error) {
+    return res.status(400).json({ error: validationResult.error.message });
+  }
+
+  planets = planets.map((p) => (p.id === Number(id) ? { ...p, name } : p));
+
+  console.log(planets);
+
+  res.status(200).json({ msg: "El planeta fue actualizado" });
+});
+
+app.delete("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  planets = planets.filter((p) => p.id !== Number(id));
+
+  console.log(planets);
+  res.status(200).json({ msg: "El planeta fue eliminado." });
+});
+
 app.listen(port, () => {
-  console.log(`Se inicializa el puerto: http://localhost:${port}/`);
+  console.log(`Se inicializa el puerto: http://localhost:${port}`);
 });
